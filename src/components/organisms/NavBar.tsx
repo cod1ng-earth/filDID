f66421bf38d5bafa4e8d49dd28d43bd84eb15f4c
Stefan Adolf <stadolf@gmail.com>
2020-07-22 02:43:27 +0200

    makes it look a little nicer. custom navbar

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -1,11 +1,12 @@
 import {
-  Blockie, Dropdown, Menu, Navbar, Row, Column,
+  Blockie, Dropdown, Menu, Row, Column, Container, Responsive, Header, Image,
 } from 'decentraland-ui';
 import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
 import { useWeb3Context } from 'web3-react';
 import { Link } from '@reach/router';
 import { useIdentity } from '../../context/IdentityContext';
+import eridanosLogo from '../../img/eridanos.svg';
 
 const UserMenu = ({ account }: {account: string}) => {
   const { connectTo3box, threeBox } = useIdentity();
@@ -25,7 +26,87 @@ const UserMenu = ({ account }: {account: string}) => {
     </Row>;
 };
 
-const NavBar = () => {
+const SignInMenu = ({ onSignIn }: any) => <Menu secondary>
+        <Menu.Item className="sign-in-button" onClick={onSignIn}>
+          Sign in
+        </Menu.Item>
+      </Menu>;
+
+const NavbarBase = (props: any) => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const {
+    activePage,
+    className,
+    isSignIn,
+    isFullscreen,
+    isOverlay,
+  } = props;
+
+  let classes = 'dcl navbar';
+
+  if (toggle) {
+    classes += ' open';
+  }
+
+  if (isSignIn) {
+    classes += ' sign-in';
+  }
+
+  if (isFullscreen) {
+    classes += ' fullscreen';
+  }
+
+  if (isOverlay) {
+    classes += ' overlay';
+  }
+
+  if (className) {
+    classes += ` ${className}`;
+  }
+
+  const Logo = () => <Image src={eridanosLogo} size='mini' />;
+
+  return <div className={classes} role="navigation">
+  <Container>
+    <div className="dcl navbar-menu">
+      <Responsive
+        as={Menu}
+        secondary
+        stackable
+        minWidth={Responsive.onlyTablet.minWidth}
+      >
+        <a className="dcl navbar-logo" href="/" >
+          <Logo />
+        </a>
+        {props.leftMenu}
+      </Responsive>
+      <Responsive
+        {...Responsive.onlyMobile}
+        className="dcl navbar-mobile-menu"
+      >
+        <a className="dcl navbar-logo" href="/">
+        <Logo />
+        </a>
+        <Header
+          size="small"
+          className={`dcl active-page ${
+            toggle ? 'caret-up' : 'caret-down'
+          }`}
+          onClick={() => setToggle(!toggle)}
+        >
+          {activePage}
+        </Header>
+      </Responsive>
+    </div>
+
+    <div className="dcl navbar-account">{props.rightMenu}</div>
+  </Container>
+  <div className="mobile-menu">{props.leftMenu}</div>
+</div>;
+};
+
+const NavBar = (props:any) => {
   const {
     account, library: web3, setConnector, unsetConnector, error,
   } = useWeb3Context();
@@ -50,14 +131,12 @@ const NavBar = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [account]);
 
-  return <Navbar
-
-    onSignIn={connect}
+  return <NavbarBase
     isConnected={!!account}
     address={account || ''}
     mana={ethBalance}
     onClickAccount={() => console.log('Clicked on account menu')}
-    rightMenu={account ? <UserMenu account={account} /> : null}
+    rightMenu={account ? <UserMenu account={account} /> : <SignInMenu onSignIn={connect}/>}
     leftMenu={
       <>
         <Menu.Item as={Link} to="/">Home</Menu.Item>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Page, Header } from 'decentraland-ui';
+import {
+  Page, Header, Image, Grid,
+} from 'decentraland-ui';
 import { RouteComponentProps } from '@reach/router';
+import eridanosLogo from '../../img/eridanos.svg';
 
 const Home = (props: RouteComponentProps) => (
     <Page>
-        <Header>Hello.</Header>
+        <Grid verticalAlign='middle' centered >
+            <Grid.Row columns={2}>
+                <Grid.Column align="right" width={3}>
+                    <Image src={eridanosLogo} wrapped />
+                </Grid.Column>
+                <Grid.Column width={3} >
+                    <Header size="large">
+                        eridanos.
+                    </Header>
+                    <Header size="small" >
+                        verifiable claims on ceramic.
+                    </Header>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     </Page>
 );
 
