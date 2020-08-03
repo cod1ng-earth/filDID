71352065b8cef460efaeed1ff85015364b4ff886
Stefan Adolf <stadolf@gmail.com>
2020-08-03 16:22:09 +0200

    adds identity wallet #develop submodule as build dependency

--- tools/build-deps.sh
+++ tools/build-deps.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-yarn
+git submodule update --init --recursive
+
+#npm install -g lerna
+cd lib/identity-wallet-js && npm install && npx tsc -p .
+yarn link
+cd ../..
+yarn link identity-wallet
