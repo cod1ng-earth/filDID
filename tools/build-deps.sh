cbd68f72abec03c627a6a375b6e1826d822f321c
Stefan Adolf <stadolf@gmail.com>
2020-08-03 19:14:13 +0200

    fix: added js-ceramic upstream and rebuild (needed for cross-fetch)

--- tools/build-deps.sh
+++ tools/build-deps.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 git submodule update --init --recursive
 
-#npm install -g lerna
-cd lib/identity-wallet-js && npm install && npx tsc -p .
-yarn link
+npm install -g lerna
+cd lib/js-ceramic && npm run bootstrap && npm run build
+cd packages/ceramic-core && yarn link
+cd ../ceramic-doctype-three-id && yarn link
+cd ../ceramic-doctype-tile && yarn link
+cd ../../../..
+cd lib/identity-wallet-js && npm install && npx tsc -p . && yarn link
 cd ../..
+
+yarn link @ceramicnetwork/ceramic-core
+yarn link @ceramicnetwork/ceramic-doctype-three-id
+yarn link @ceramicnetwork/ceramic-doctype-tile
 yarn link identity-wallet
